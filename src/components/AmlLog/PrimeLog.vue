812b800e1ad0851452205b2c439e85a4a6cbf0d4
009647 <asaki0510@megabank.com.tw>
2018-05-25 15:19:59 +0800

    修正小問題

--- src/components/AmlLog/PrimeLog.vue
+++ src/components/AmlLog/PrimeLog.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="sas-log-app container">
-    <h1 class="text-center">AS400 SWIFT WIRE</h1>   
+    <h1 class="text-center">AS400 SWIFT WIRE</h1>
     <div class="row">
         <div class="form-group">
             <div class="col-md-2 col-md-offset-2">
                 <p>400 Library</p>
                 <select class="form-control" v-model="selectedLibrary" @change="changeSelect()">
-                    <option v-for="item in libraryList" :key="item.branch">{{item.branch}}</option>        
-                </select>                  
+                    <option v-for="item in libraryList" :key="item.branch">{{item.branch}}</option>
+                </select>
             </div>
             <div class="col-md-2">
-                <p>Swift In Out</p>                
+                <p>Swift In Out</p>
                 <select class="form-control" v-model="swiftInOut" @change="changeSelect()">
                     <option>In</option>
                     <option>Out</option>
                 </select>
             </div>
             <div class="col-md-2">
-                <p>Ref No</p>      
+                <p>Ref No</p>
                 <input id="refinput" class="form-control" type="text" placeholder="Type to search..." autocomplete="off">
                 <typeahead v-model="model" target="#refinput" :async-function="(swiftInOut == 'Out' ? getSwtOutQuery : getSwtInQuery)" item-key="OUR_REF_NO">
                 <template slot="item" slot-scope="props">
@@ -27,40 +27,40 @@
                     </a>
                     </li>
                 </template>
-                </typeahead>                           
+                </typeahead>
                 <!-- <span class="glyphicon glyphicon-refresh spinning" v-if="refLoading"></span>
-                <select class="form-control" v-model="refText" v-if="!refLoading">                   
-                    <option v-for="item in swiftRefList" :key="item.FD300_OUR_REF_NO" v-if="swiftInOut == 'Out'">{{item.FD300_OUR_REF_NO}}</option>        
-                    <option v-for="item in swiftRefList" :key="item.FD200_OUR_REF_NO" v-if="swiftInOut == 'In'">{{item.FD200_OUR_REF_NO}}</option>                            
-                </select>           -->
+                <select class="form-control" v-model="refText" v-if="!refLoading">
+                    <option v-for="item in swiftRefList" :key="item.FD300_OUR_REF_NO" v-if="swiftInOut == 'Out'">{{item.FD300_OUR_REF_NO}}</option>
+                    <option v-for="item in swiftRefList" :key="item.FD200_OUR_REF_NO" v-if="swiftInOut == 'In'">{{item.FD200_OUR_REF_NO}}</option>
+                </select>-->
             </div>
             <div class="col-md-2">
                 <p>搜尋</p>
                 <button class="btn btn-primary" @click="searchLog">Search</button>
             </div>
-        </div>   
-    </div> 
-        <h1 class="text-center" v-if="swiftInOut == 'Out'">D300</h1> 
-        <h1 class="text-center" v-if="swiftInOut == 'In'">D200</h1> 
-        <br />        
-        <h1 class="text-center">Swallow</h1>         
+        </div>
+    </div>
+        <h1 class="text-center" v-if="swiftInOut == 'Out'">D300</h1>
+        <h1 class="text-center" v-if="swiftInOut == 'In'">D200</h1>
+        <br />
+        <h1 class="text-center">Swallow</h1>
         <p v-if="tableLoading" class="text-center"><img src="../../../data/img/loading.gif" alt=""></p>
         <div v-if="tableLoading == false">
             <datagrid
             :data="swallowList"
             :columns="swallowColumns"
             :filter-key="swallowSearchQuery"
-            ></datagrid>               
+            ></datagrid>
         </div>
-        <br />        
-        <h1 class="text-center">Prime</h1>                 
+        <br />
+        <h1 class="text-center">Prime</h1>
         <p v-if="tableLoading" class="text-center"><img src="../../../data/img/loading.gif" alt=""></p>
         <div v-if="tableLoading == false">
             <datagrid
             :data="primeList"
             :columns="primeColumns"
             :filter-key="swallowSearchQuery"
-            ></datagrid>               
+            ></datagrid>
         </div>
   </div>
 </template>
@@ -90,24 +90,24 @@ export default {
             "branchCode":"0M2"
         }],
         swiftRefList: {},
-        swiftInOut: "Out",       
-        selectedLibrary: "TLOSANGEL", 
-        selectedBrnCode: "0A4",    
-        selectedShortBrn: "LAC",                   
-        refText: "",       
+        swiftInOut: "Out",
+        selectedLibrary: "TLOSANGEL",
+        selectedBrnCode: "0A4",
+        selectedShortBrn: "LAC",
+        refText: "",
         refLoading: true,
-        tableLoading: false,      
-        swallowSearchQuery: "",        
-        swallowList: [],  
-        swallowColumns: ['TAG_20', 'SW_UMID' , 'AML_RESULT', 'MESG_TYPE','AML_SEND_DATE_TIME','AML_ACK_DATE_TIME'],        
+        tableLoading: false,
+        swallowSearchQuery: "",
+        swallowList: [],
+        swallowColumns: ['TAG_20', 'SW_UMID' , 'AML_RESULT', 'MESG_TYPE','AML_SEND_DATE_TIME','AML_ACK_DATE_TIME'],
         primeList:[],
         primeColumns: ['SeqNumb','Ref','UserMessageReference','Source','Dept', 'ReqTime' , 'ConfirmState', 'ConfirmTime'],
         model:""
     }
     },
-    methods: {        
-        searchLog: function () {   
-            this.tableLoading = true   
+    methods: {
+        searchLog: function () {
+            this.tableLoading = true
             this.swallowSearchQuery = ""
             for(var i=0;i<this.libraryList.length;i++){
                 if(this.selectedLibrary == this.libraryList[i].branch){
@@ -117,59 +117,61 @@ export default {
             }
 
             if(this.swiftInOut === "Out"){
-                this.getSwallowSwtOut()                
+                this.getSwallowSwtOut()
             }else{
                 this.getSwallowSwtIn()
             }
         },
-        getSwallowSwtOut: function () {   
-            var queryRef = (this.model.OUR_REF_NO == undefined ? this.model : this.model.OUR_REF_NO)            
+        getSwallowSwtOut: function () {
+            var queryRef = (this.model.OUR_REF_NO == undefined ? this.model : this.model.OUR_REF_NO)
             axios.get('/primeapi/swallowlog/out/' + this.selectedBrnCode + "/" + queryRef)
             .then((resp) => {
                 this.swallowList = resp.data[0]
-                this.tableLoading = false   
-                this.primeList = []                
+                this.tableLoading = false
+                this.primeList = []
                 for(let i=0;i<this.swallowList.length;i++){
-                    this.getPrimeLog(this.swallowList[0].SW_UMID)                
+                    this.getPrimeLog(this.swallowList[0].SW_UMID)
                 }
             })
             .catch((err) => {
                 console.log(err)
-                this.tableLoading = false   
+                this.tableLoading = false
             })
         },
-        getSwallowSwtIn: function () {   
+        getSwallowSwtIn: function () {
             var queryRef = (this.model.OUR_REF_NO == undefined ? this.model : this.model.OUR_REF_NO)
             axios.get('/primeapi/swallowlog/in/' + this.selectedBrnCode + "/" + queryRef)
             .then((resp) => {
                 this.swallowList = resp.data[0]
-                this.tableLoading = false   
+                this.tableLoading = false
                 this.primeList = []
                 for(let i=0;i<this.swallowList.length;i++){
-                    this.getPrimeLog(this.swallowList[0].SW_UMID)                
+                    this.getPrimeLog(this.swallowList[0].SW_UMID)
                 }
             })
             .catch((err) => {
                 console.log(err)
-                this.tableLoading = false   
+                this.tableLoading = false
             })
         },
-        getPrimeLog: function (ref) {   
+        getPrimeLog: function (ref) {
             axios.get('/primeapi/primelog/' + this.selectedShortBrn + "/" + ref)
             .then((resp) => {
-                // this.primeList = resp.data[0][0]
+                if(resp.data[0][0] == undefined){
+                  return;
+                }
                 this.primeList.push(resp.data[0][0])
-                this.tableLoading = false   
+                this.tableLoading = false
                 console.log(this.primeList)
             })
             .catch((err) => {
                 console.log(err)
-                this.tableLoading = false   
+                this.tableLoading = false
             })
         },
         getSwtOutQuery (query, done) {
         query = query.toUpperCase()
-        axios.get('/primeapi/as400SwiftOut/' + this.selectedLibrary + '/' + query)        
+        axios.get('/primeapi/as400SwiftOut/' + this.selectedLibrary + '/' + query)
           .then(res => {
             done(res.data)
           })
@@ -179,7 +181,7 @@ export default {
         },
         getSwtInQuery (query, done) {
         query = query.toUpperCase()
-        axios.get('/primeapi/as400SwiftIn/' + this.selectedLibrary + '/' + query)        
+        axios.get('/primeapi/as400SwiftIn/' + this.selectedLibrary + '/' + query)
           .then(res => {
             done(res.data)
           })
@@ -192,7 +194,7 @@ export default {
         }
     },
     mounted () {
-        
+
     },
     components: {
         datagrid,
